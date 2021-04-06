1cae2241457c0ec068504195c6cecaf48e37c379
Bossfire964 <74617357+Bossfire964@users.noreply.github.com>
2021-04-06 15:22:27 -0400

    Update FileSystem.cs

--- FileSystem.cs
+++ FileSystem.cs
@@ -1,32 +1,33 @@
 ﻿using System;
 using System.IO;
 
+//This is not needed but only used to test on mac and windows
 
 namespace FileSystem
 {
     public class filesystem
     {
-        public static bool isWin = true;
+        public static bool isWin = true; //Checks if we are on windows or mac
 
-        public string combineFile(string[] directs, string orgin)
+        public string combineFile(string[] directs, string orgin). //used alot for filesystems
         {
             if (isWin == true)
             {
                 string newpath = orgin;
                 foreach (string direct in directs)
                 {
-                    newpath = newpath + @"\" + direct;
+                    newpath = newpath + @"\" + direct; //makes path for windows
                 }
-                return newpath;
+                return newpath; //return path
 
             } else
             {
                 string newpath = orgin;
                 foreach (string direct in directs)
                 {
-                    newpath = newpath + "/" + direct;
+                    newpath = newpath + "/" + direct; //makes path for mac
                 }
-                return newpath;
+                return newpath; //returns path
             }
         }
 
