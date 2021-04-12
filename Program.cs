a706f40f9b002f55a6724475f2385e86e6f7cb2a
Bossfire964 <74617357+Bossfire964@users.noreply.github.com>
2021-04-12 10:18:23 -0400

    Update Program.cs
    Added comments

--- Program.cs
+++ Program.cs
@@ -10,48 +10,48 @@ namespace _5._0WindowsBrawlModder
 {
     public class Program
     {
-        public static Functions function = new Functions(); //functions connection
-        public static string input;
-        public static bool isipa = true;
-        static void Main(string[] args)
+        public static Functions function = new Functions(); //functions connection for main actions
+        public static string input; //main input of the file, public to everyone
+        public static bool isipa = true; //if file is ipa or apk, avalible for everyone
+        static void Main(string[] args) //main function
         {
-            filesystem FileSystem = new filesystem(); //all of this deletes any leftover files from last session
-            if (Directory.Exists(FileSystem.combineFile(new string[] {"Payload"}, Directory.GetCurrentDirectory()))) {
-                Directory.Delete(FileSystem.combineFile(new string[] { "Payload" }, Directory.GetCurrentDirectory()), true);
+            filesystem FileSystem = new filesystem(); //connects to the filesystem
+            if (Directory.Exists(FileSystem.combineFile(new string[] {"Payload"}, Directory.GetCurrentDirectory()))) { //if Payload folder exists
+                Directory.Delete(FileSystem.combineFile(new string[] { "Payload" }, Directory.GetCurrentDirectory()), true); //deletes Payload
             }
-            if (Directory.Exists(FileSystem.combineFile(new string[] { "rebrawl-classic-104" }, Directory.GetCurrentDirectory())))
+            if (Directory.Exists(FileSystem.combineFile(new string[] { "rebrawl-classic-104" }, Directory.GetCurrentDirectory()))) //if rebrawl-classic-104 exists
             {
-                Directory.Delete(FileSystem.combineFile(new string[] { "rebrawl-classic-104" }, Directory.GetCurrentDirectory()), true);
+                Directory.Delete(FileSystem.combineFile(new string[] { "rebrawl-classic-104" }, Directory.GetCurrentDirectory()), true); //deletes rebrawl-classic-104
             }
-            if (File.Exists(FileSystem.combineFile(new string[] { "brawlstars.zip" }, Directory.GetCurrentDirectory())))
+            if (File.Exists(FileSystem.combineFile(new string[] { "brawlstars.zip" }, Directory.GetCurrentDirectory()))) //if brawlstars.zip exists
             {
-                File.Delete(FileSystem.combineFile(new string[] { "brawlstars.zip" }, Directory.GetCurrentDirectory()));
+                File.Delete(FileSystem.combineFile(new string[] { "brawlstars.zip" }, Directory.GetCurrentDirectory())); //deletes brawlstars.zip
             }
-            bool answer = false;
-            while (answer == false) //main menu
+            bool answer = false; //variable for while loop
+            while (answer == false) //main menu loop
             {
-                function.Startmessage();
-                input = Console.ReadLine();
-                answer = function.StartMessageAction(input);
+                function.Startmessage(); //prints message
+                input = Console.ReadLine(); //reads input
+                answer = function.StartMessageAction(input); // connects to action and sets answer to true or false
             }
 
-            bool biganswer = false;
+            bool biganswer = false; //loop variable for main editing
             
             while (biganswer == false) //main loop for editing
             {
-                answer = false;
+                answer = false; //variable for mini loop for brawler edits and importing maps
                 while (answer == false) //brawler edit or import map options
                 {
-                    function.MainMessage();
-                    input = Console.ReadLine();
-                    answer = function.MainMessageAction(input);
+                    function.MainMessage(); //displays message 
+                    input = Console.ReadLine();  //reads input
+                    answer = function.MainMessageAction(input);// does action and sets answer to either true or false
                 }
-                answer = false;
+                answer = false; //variable for mini loop brawler edit
                 while (answer == false) // brawler edit screen
                 {
-                    function.BrawlEditMessage();
-                    input = Console.ReadLine();
-                    answer = function.BrawlEditAction(input);
+                    function.BrawlEditMessage(); //displays message 
+                    input = Console.ReadLine(); //reads input
+                    answer = function.BrawlEditAction(input); //does action and sets answer to true or false
                 }
             }
             
